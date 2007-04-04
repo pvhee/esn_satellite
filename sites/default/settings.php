30b98e92a8383d8a9c7a58167c900ee960fdaaba
pvhee <pvhee@724f02df-c632-4e49-bb9a-5d3e6873b890>
2007-04-04 21:30:36 +0000

    release notes added + default database config restored
    git-svn-id: http://svn.youthagora.org/satellite/trunk@2 724f02df-c632-4e49-bb9a-5d3e6873b890

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -84,7 +84,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysql://pvhee:lapjeskat@localhost/my_satellite';
+$db_url = 'mysql://username:password@localhost/databasename';
 $db_prefix = 'esnsatellite_';
 
 /**
