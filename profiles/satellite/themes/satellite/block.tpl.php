15cf8e854a62684dfb2304942470b11e516c2fdc
antonio <antonio@724f02df-c632-4e49-bb9a-5d3e6873b890>
2009-03-20 11:05:37 +0000

    #93 Style Blocks: Done. New dump.
    git-svn-id: http://svn.youthagora.org/satellite/trunk@187 724f02df-c632-4e49-bb9a-5d3e6873b890

--- profiles/satellite/themes/satellite/block.tpl.php
+++ profiles/satellite/themes/satellite/block.tpl.php
@@ -1,4 +1,4 @@
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?> orange">
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?>">
 
 <?php if (!empty($block->subject)): ?>
   <h2><?php print $block->subject ?></h2>
