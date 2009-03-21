ce48c1bdef3e98e53621899984e990c7a0d5f532
antonio <antonio@724f02df-c632-4e49-bb9a-5d3e6873b890>
2009-03-21 14:05:12 +0000

    #125 Block edit link: closed
    git-svn-id: http://svn.youthagora.org/satellite/trunk@210 724f02df-c632-4e49-bb9a-5d3e6873b890

--- profiles/satellite/themes/satellite/template.php
+++ profiles/satellite/themes/satellite/template.php
@@ -5,3 +5,19 @@ function satellite_get_ie_styles() {
   // $css .= '<style type="text/css" media="all">@import "'. base_path() . path_to_theme() .'/fix-ie-rtl.css";</style>';
   return $css;
 }
+
+/**
+ * Print edit block link in block templates files
+ *
+ * @param $block
+ * @return Block link
+ */
+function satellite_block_edit_link(&$block) {
+  $out = '';
+  if (user_access('administer blocks')) {
+    $out = '<div class="block-edit-link">';
+    $out .= l(t('Edit Block'), 'admin/build/block/configure/' . $block->module . '/' . $block->delta . '?' . drupal_get_destination());
+    $out .= '</div>';
+  }
+  return $out;
+}
