8b686ea406498709e55e2ebb7a693cdbc546d8b8
antonio <antonio@724f02df-c632-4e49-bb9a-5d3e6873b890>
2009-03-21 13:30:12 +0000

    New navigation color. Link edit block.
    git-svn-id: http://svn.youthagora.org/satellite/trunk@209 724f02df-c632-4e49-bb9a-5d3e6873b890

--- profiles/satellite/themes/satellite/blocktheme-orange.tpl.php
+++ profiles/satellite/themes/satellite/blocktheme-orange.tpl.php
@@ -1,5 +1,21 @@
 <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?> orange">
 	<div class="inner">
+		<?php
+		  // print out an 'Edit Block' link for users with administer blocks privilege
+		  if ($block->module == 'block'):
+		    if (user_access('administer blocks')) :?>
+		    <div class="block-tabs-wrapper">
+		      <ul class="tabs">
+		        <li>
+		          <?php print l('Edit Block','admin/build/block/configure/' . $block->module . '/' . $block->delta.'?'.drupal_get_destination()); ?>
+		        </li>
+		      </ul>
+		    </div>
+		    <?php
+		    endif;
+		  endif;
+		?>		
+		
 	<?php if (!empty($block->subject)): ?>
 	  <h2><?php print $block->subject ?></h2>
 	<?php endif;?>
