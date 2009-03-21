3aa6fc563efc97cfb02f11986d9c4fdcf3f90325
antonio <antonio@724f02df-c632-4e49-bb9a-5d3e6873b890>
2009-03-21 04:29:18 +0000

    New regions done. Footer CSS. Other minor CSS.
    git-svn-id: http://svn.youthagora.org/satellite/trunk@199 724f02df-c632-4e49-bb9a-5d3e6873b890

--- profiles/satellite/themes/satellite/template.php
+++ profiles/satellite/themes/satellite/template.php
@@ -1,2 +1,7 @@
 <?php
 
+function satellite_get_ie_styles() {
+  // $index = rand() / 4;
+  // $css .= '<style type="text/css" media="all">@import "'. base_path() . path_to_theme() .'/fix-ie-rtl.css";</style>';
+  return $css;
+}
