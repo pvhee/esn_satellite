0cb44e99e9648745f20acf440500cf00c4d8c62f
antonio <antonio@724f02df-c632-4e49-bb9a-5d3e6873b890>
2009-03-21 14:27:53 +0000

    #123 inline sub-sub navigation (not main menu) is hidden by the theme colors: fixed.
    git-svn-id: http://svn.youthagora.org/satellite/trunk@213 724f02df-c632-4e49-bb9a-5d3e6873b890

--- profiles/satellite/themes/satellite/blocktheme-orange.tpl.php
+++ profiles/satellite/themes/satellite/blocktheme-orange.tpl.php
@@ -1,6 +1,6 @@
 <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?> orange">
 	<div class="inner">
-
+	<?php print satellite_block_edit_link($block); ?>
 		
 	<?php if (!empty($block->subject)): ?>
 	  <h2><?php print $block->subject ?></h2>
