d5a0ac0cc5402c32c3fbe1a02a889f6e3534cf9c
antonio <antonio@724f02df-c632-4e49-bb9a-5d3e6873b890>
2009-03-22 16:00:18 +0000

    Nav menu fix.
    git-svn-id: http://svn.youthagora.org/satellite/trunk@239 724f02df-c632-4e49-bb9a-5d3e6873b890

--- profiles/satellite/themes/satellite/template.php
+++ profiles/satellite/themes/satellite/template.php
@@ -16,8 +16,22 @@ function satellite_block_edit_link(&$block) {
   $out = '';
   if (user_access('administer blocks')) {
     $out = '<div class="block-edit-link">';
-    $out .= l(t('Edit Block'), 'admin/build/block/configure/' . $block->module . '/' . $block->delta, array('query' => drupal_get_destination()));
+    $out .= l(t('Edit Block'), 'admin/build/block/configure/' . $block->module . '/' . $block->delta, array('query' => drupal_get_destination() ));
     $out .= '</div>';
   }
   return $out;
+}
+
+/**
+ * Insert a span in every link for a better styling.
+ *
+ * @param $link
+ * @return item link
+ */
+function satellite_menu_item_link($link) {
+  if (empty($link['localized_options'])) {
+    $link['localized_options'] = array( );
+  }
+  $link['localized_options']['html'] = true;
+  return l('<span>' . $link['title'] . '</span>', $link['href'], $link['localized_options']);
 }
