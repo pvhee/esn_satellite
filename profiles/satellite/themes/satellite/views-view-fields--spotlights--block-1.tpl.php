89b789b549c91b48e36e64644b3234686b0b4508
antonio <antonio@724f02df-c632-4e49-bb9a-5d3e6873b890>
2009-03-24 00:45:56 +0000

    Spotlight style.
    git-svn-id: http://svn.youthagora.org/satellite/trunk@281 724f02df-c632-4e49-bb9a-5d3e6873b890

---
+++ profiles/satellite/themes/satellite/views-view-fields--spotlights--block-1.tpl.php
@@ -0,0 +1,64 @@
+<?php
+// $Id: views-view-fields.tpl.php,v 1.6 2008/09/24 22:48:21 merlinofchaos Exp $
+/**
+ * @file views-view-fields.tpl.php
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->separator: an optional separator that may appear before a field.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php 
+  $background = $fields['field_image_fid']->content;
+//  if (!is_file($background)) {
+//  	global $theme;
+//  	$background = drupal_get_path('theme', $theme).'/images/spotlight_default.jpg';
+//  }
+  unset($fields['field_image_fid']);
+?>
+
+<div class="wrapper-out">
+  <div class="spotlight-image" style="<?php if ($background) print "background: url('$background') no-repeat 0 0;"; ?>">
+    <div class="black-text">
+      <div class="inner">
+        <?php 
+          print $fields['field_text_spotlight_value']->content;
+          unset($fields['field_text_spotlight_value']);
+        ?>      
+      </div>
+    </div>
+  </div>
+    <div class="wrapper-in">
+      <div class="spotlight-content">
+	      <?php foreach ($fields as $id => $field): ?>
+	        <?php if (!empty($field->separator)): ?>
+	          <?php print $field->separator; ?>
+	        <?php endif; ?>
+	      
+	        <<?php print $field->inline_html;?> class="views-field-<?php print $field->class; ?>">
+	          <?php if ($field->label): ?>
+	            <label class="views-label-<?php print $field->class; ?>">
+	              <?php print $field->label; ?>:
+	            </label>
+	          <?php endif; ?>
+	            <?php
+	            // $field->element_type is either SPAN or DIV depending upon whether or not
+	            // the field is a 'block' element type or 'inline' element type.
+	            ?>
+	            <<?php print $field->element_type; ?> class="field-content"><?php print $field->content; ?></<?php print $field->element_type; ?>>
+	        </<?php print $field->inline_html;?>>
+	      <?php endforeach; ?>
+      </div>
+    </div>
+</div>
