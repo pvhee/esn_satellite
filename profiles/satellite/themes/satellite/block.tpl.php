b1d83d9fc155ca2710541af259468810140122c4
antonio <antonio@724f02df-c632-4e49-bb9a-5d3e6873b890>
2009-04-11 08:55:43 +0000

    CSS fix. BLock and h2.
    git-svn-id: http://svn.youthagora.org/satellite/trunk@366 724f02df-c632-4e49-bb9a-5d3e6873b890

--- profiles/satellite/themes/satellite/block.tpl.php
+++ profiles/satellite/themes/satellite/block.tpl.php
@@ -1,4 +1,4 @@
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?>">
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?> blue">
 <div class="inner">
 	<?php print satellite_block_edit_link($block); ?>
 	
