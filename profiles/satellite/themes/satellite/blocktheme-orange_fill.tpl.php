6c2227850e05c29e890c3ff76981784cede6a504
antonio <antonio@724f02df-c632-4e49-bb9a-5d3e6873b890>
2009-04-15 15:12:52 +0000

    #310 Blocks style messed up in IE6: fixed.
    git-svn-id: http://svn.youthagora.org/satellite/trunk@374 724f02df-c632-4e49-bb9a-5d3e6873b890

--- profiles/satellite/themes/satellite/blocktheme-orange_fill.tpl.php
+++ profiles/satellite/themes/satellite/blocktheme-orange_fill.tpl.php
@@ -1,4 +1,4 @@
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?> orange fill">
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?> orange-fill">
 	<div class="inner">
 		<?php print satellite_block_edit_link($block); ?>
 		
--- profiles/satellite/themes/satellite/blocktheme-pink_fill.tpl.php
+++ profiles/satellite/themes/satellite/blocktheme-pink_fill.tpl.php
@@ -1,4 +1,4 @@
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?> pink fill">
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block block-<?php print $block->module ?> pink-fill">
 	<div class="inner">
 		<?php print satellite_block_edit_link($block); ?>
 		
